3ff9f86b169f0db8da5debb0a497e2174bec323a
Luca Iocchi <iocchi@diag.uniroma1.it>
2025-09-15 22:20:14 +0200

    first commit: marr_gz package

---
+++ marr_gz/launch/marr_robot.launch.py
@@ -0,0 +1,290 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, LogInfo
+from launch.actions import RegisterEventHandler, SetEnvironmentVariable, ExecuteProcess
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, EnvironmentVariable, TextSubstitution, PythonExpression
+from launch.conditions import IfCondition
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    # Launch Arguments
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    gz_args = LaunchConfiguration('gz_args', default='[]')
+    world_file = LaunchConfiguration('world_file')   
+    robot_type = LaunchConfiguration('robot_type')
+    dof = LaunchConfiguration('dof')
+    robot_name = LaunchConfiguration('robot_name')
+    control_interface = LaunchConfiguration('control_interface')
+    imu = LaunchConfiguration('imu')
+    lidar = LaunchConfiguration('lidar')
+    rgb = LaunchConfiguration('rgb')
+    rgbd = LaunchConfiguration('rgbd')
+    arms = LaunchConfiguration('arms')
+    pantilt = LaunchConfiguration('pantilt')
+
+    pkg_share = FindPackageShare('marr_gz')
+
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value=use_sim_time,
+        description='If true, use simulated clock.'
+    )
+
+    world_file_arg = DeclareLaunchArgument(
+        'world_file',
+        default_value=TextSubstitution(text='empty.world'),
+        description='Gazebo world to launch.'
+    )
+
+    robot_type_arg = DeclareLaunchArgument(
+        'robot_type',
+        default_value=TextSubstitution(text='wheeled'),
+        description='Type of the robot to spawn [wheeled|reacher].'
+    )
+
+    dof_arg = DeclareLaunchArgument(
+        'dof',
+        default_value=TextSubstitution(text='2'),
+        description='DOF of reacher arm [2...6]'
+    )
+
+    robot_name_arg = DeclareLaunchArgument(
+        'robot_name',
+        default_value=TextSubstitution(text='robot1'),
+        description='Name (i.e., prefix) of the robot to spawn.'
+    )
+
+    control_interface_arg = DeclareLaunchArgument(
+        'control_interface',
+        default_value=TextSubstitution(text='effort'),
+        description='control interface [effort|velocity|position]'
+    )
+
+    imu_arg = DeclareLaunchArgument(
+        'imu',
+        default_value=TextSubstitution(text='False'),
+        description='Enable IMU sensor'
+    )
+
+    lidar_arg = DeclareLaunchArgument(
+        'lidar',
+        default_value=TextSubstitution(text='False'),
+        description='Enable lidar sensor'
+    )
+
+    rgb_arg = DeclareLaunchArgument(
+        'rgb',
+        default_value=TextSubstitution(text='False'),
+        description='Enable RGB camera'
+    )
+
+    rgbd_arg = DeclareLaunchArgument(
+        'rgbd',
+        default_value=TextSubstitution(text='False'),
+        description='Enable RGBD camera'
+    )
+
+    arms_arg = DeclareLaunchArgument(
+        'arms',
+        default_value=TextSubstitution(text='False'),
+        description='Enable robot arms for wheeled robot'
+    )
+
+    pantilt_arg = DeclareLaunchArgument(
+        'pantilt',
+        default_value=TextSubstitution(text='False'),
+        description='Enable pan-tilt device for wheeled robot (to mount cameras on)'
+    )
+
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [ pkg_share, 'urdf', 
+                    PythonExpression( ["'", robot_type, ".urdf.xacro'" ]) ]
+            ),
+            ' ',
+            PythonExpression( ["'use_imu:=", imu, "'" ]),
+            ' ',
+            PythonExpression( ["'use_lidar:=", lidar, "'" ]),
+            ' ',
+            PythonExpression( ["'use_rgb:=", rgb, "'" ]),
+            ' ',
+            PythonExpression( ["'use_rgbd:=", rgbd, "'" ]),
+            ' ',
+            PythonExpression( ["'use_arms:=", arms, "'" ]),
+            ' ',
+            PythonExpression( ["'use_pantilt:=", pantilt, "'" ]),
+            ' ',
+            PythonExpression( ["'dof:=", dof, "'" ]),
+        ]
+    )
+
+    robot_description = {'robot_description': robot_description_content}
+    
+    has_wheels = PythonExpression(["'", robot_type, "' == 'wheeled'" ])
+
+    has_arms = PythonExpression([arms, " or ", "'", robot_type, "' == 'reacher'" ])
+
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-topic', 'robot_description',
+                   '-name', robot_type, '-allow_renaming', 'true'],
+    )
+
+    detach_cmd = ExecuteProcess(
+        cmd=[ "gz topic -t '/B1/detach' -m gz.msgs.Empty -p 'unused: true'" ],
+        shell=True,
+        condition=IfCondition(has_arms),
+    )
+
+    robot_controllers_file = PythonExpression(["'", robot_type, "' + ('", dof, "' if '", robot_type, "' == 'reacher' else '')", " + '_controllers.yaml'" ])
+
+    robot_controllers = PathJoinSubstitution(
+        [ pkg_share, 'config', robot_controllers_file ]
+    )
+
+    pantilt_controllers = PathJoinSubstitution(
+        [ pkg_share, 'config', 'pantilt_controllers.yaml' ]
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+    )
+
+    ddrive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'ddrive_controller',
+            '--param-file',
+            robot_controllers,
+            ],
+        condition=IfCondition(has_wheels),
+    )
+
+    arm_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            PythonExpression( ["'arm_", control_interface, "_controller'" ] ),
+            '--param-file',
+            robot_controllers,
+            ],
+        condition=IfCondition(has_arms),
+    )
+
+    pantilt_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            PythonExpression( ["'pantilt_", control_interface, "_controller'" ] ),
+            '--param-file',
+            pantilt_controllers,
+            ],
+        condition=IfCondition(pantilt),
+    )
+
+    # Check ga service info
+    #   gz service -s <service> -i
+    # Chech ROS2 service interface
+    #   ros2 interface show ros_gz_interfaces/srv/SpawnEntity
+
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+                   '/world/default/create@ros_gz_interfaces/srv/SpawnEntity@gz.msgs.EntityFactory@gz.msgs.Boolean',
+                   '/world/default/remove@ros_gz_interfaces/srv/DeleteEntity@gz.msgs.Entity@gz.msgs.Boolean',
+            '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU',
+            '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
+
+#TODO cameras
+
+            ],
+        output='screen'
+    )
+
+
+
+    '''
+    SetEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH', [ EnvironmentVariable('GZ_SIM_RESOURCE_PATH'),
+        PathJoinSubstitution([pkg_share, 'models']) ]
+    ),
+    SetEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH', [ EnvironmentVariable('GZ_SIM_RESOURCE_PATH'),
+        PathJoinSubstitution([pkg_share, 'worlds']) ]
+    ),
+    '''
+
+    return LaunchDescription([
+        # Launch Arguments
+        use_sim_time_arg,
+        world_file_arg,
+        robot_type_arg, dof_arg,
+        robot_name_arg,
+        control_interface_arg,
+        imu_arg, lidar_arg, rgb_arg, rgbd_arg,
+        arms_arg, pantilt_arg,
+
+        # Launch gazebo environment
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
+                                       'launch',
+                                       'gz_sim.launch.py'])]),
+            launch_arguments = [ 
+                ('gz_args', [gz_args, ' -r -v 1 ', 
+                    PathJoinSubstitution([pkg_share, 'worlds/', world_file]) ]),
+                ('on_exit_shutdown', 'True' ),
+             ] ),
+
+        LogInfo(msg=PythonExpression(["'Robot controllers file: ",robot_controllers_file,"'"])),
+
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=gz_spawn_entity,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[ddrive_controller_spawner, arm_controller_spawner, pantilt_controller_spawner],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=arm_controller_spawner,
+                on_exit=[detach_cmd],
+            )
+        ),
+       
+        bridge,
+        robot_state_publisher,
+        gz_spawn_entity,
+
+    ])
+
---
+++ marr_gz/scripts/marr_launch.py
@@ -0,0 +1,79 @@
+import os, sys
+import yaml
+
+
+if len(sys.argv)<2:
+    print(f"Use: python {sys.argv[0]} <launch_config_file.yaml>")
+    sys.exit(1)
+
+configfile = sys.argv[1]
+
+with open(configfile) as cf:
+    try:
+        config = yaml.safe_load(cf)
+    except yaml.YAMLError as exc:
+        print(exc)
+        sys.exit(1)
+
+print(config)
+
+rconfig = config['robot']
+
+world_file_arg = f"world_file:=empty.world"
+
+rbase = rconfig['base']
+
+
+def get_config(key):
+    if key in rconfig.keys():
+        return rconfig[key]
+    else:
+        return None
+
+def get_arg(value,key):
+
+    s = ""
+    v = get_config(key)
+    if v is not None:
+        v = rconfig[key]
+        s = f"{value}:={v}"
+
+    return s 
+
+
+robot_type_arg = get_arg("robot_type","base")
+robot_name_arg = "robot_name:=robot1"
+control_interface_arg = get_arg("control_interface", "arms_control_interface")
+imu_arg = get_arg("imu","imu")
+lidar_arg = get_arg("lidar","lidar")
+rgb_arg = "rgb:=True" if get_config("camera")=="rgb" else ""
+rgbd_arg = "rgbd:=True" if get_config("camera")=="rgbd" else ""
+
+if rbase=='wheeled':
+
+    arms_arg = f"arms:={rconfig['arms']}"
+    pantilt_arg = f"pantilt:={rconfig['pantilt']}"
+
+    launch_cmd = f"ros2 launch marr_gz marr_robot.launch.py {world_file_arg} {robot_type_arg} " + \
+      f"{robot_name_arg} {control_interface_arg} {imu_arg} {lidar_arg} " + \
+      f"{rgb_arg} {rgbd_arg} {arms_arg} {pantilt_arg}" 
+
+
+elif rbase=='reacher':
+
+    dof = int(rconfig['dof'])
+    if dof<2 or dof>6:
+        print(f"Reacher with {dof} dof not implemented.")
+        sys.exit(1)
+    dof_arg = f"dof:={dof}"
+
+    launch_cmd = f"ros2 launch marr_gz marr_robot.launch.py {world_file_arg} {robot_type_arg} " + \
+      f"{robot_name_arg} {control_interface_arg} {imu_arg} {lidar_arg} " + \
+      f"{rgb_arg} {rgbd_arg}" 
+
+else:
+    print(f"Unknown robot base {rbase}")
+    sys.exit(1)
+
+os.system(f"cd ~/ros2_ws && colcon build && {launch_cmd}")
+
